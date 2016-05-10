b1c656d802d17d5bbca2c959a4913571598a8c67
gerasimvol <volodik1802@gmail.com>
2016-05-10 15:26:36 +0300

    loading bar added

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,4 +1,4 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['angular-loading-bar']);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
     console.log("Hello World from controller");
     
